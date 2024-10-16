8487d77b3e49c3e9b0e7940cb7442f795dbb69dd
platard <platard@algonquincollege.com>
2024-10-15 21:34:16 -0400

    Refactor image paths in index.html and main.js

---
+++ week7/starter/main.js
@@ -0,0 +1,60 @@
+// Variables
+const products = [
+  {
+    id: 2,
+    title: "Ottawa T-Shirt",
+    basePrice: 7.99,
+    materials: {
+      cotton: 7.99,
+      "organic cotton": 9.99
+    },
+    image: "dark-green-t-shirt-parliament-hill.webp"
+  },
+  {
+    id: 3,
+    title: "Montreal T-Shirt",
+    basePrice: 7.99,
+    materials: {
+      cotton: 7.99,
+      "dry-fit": 9.99
+    },
+    image: "dark-navy-blue-t-shirt-montreal.webp"
+  },
+  {
+    id: 4,
+    title: "Landscape T-Shirt",
+    basePrice: 7.99,
+    materials: {
+      polyester: 7.99,
+      "eco-blend": 10.99
+    },
+    image: "green-t-shirt-landscape.webp"
+  },
+  {
+    id: 5,
+    title: "Quebec City T-Shirt",
+    basePrice: 7.99,
+    materials: {
+      cotton: 7.99,
+      "organic cotton": 9.99,
+      "hemp blend": 10.99
+    },
+    image: "light-gray-t-shirt-quebec-city.webp"
+  },
+  {
+    id: 6,
+    title: "Beaver T-Shirt",
+    basePrice: 7.99,
+    materials: {
+      cotton: 7.99,
+      polyester: 8.49,
+      "dry-fit": 9.99
+    },
+    image: "maroon-t-shirt-beaver.webp"
+  }
+];
+
+// Display products
+
+
+// Refactor to include dinamic properties
